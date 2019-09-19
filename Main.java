be3bbd08a57fc96d56dc86b52b4fe3c87b6e2b2a
Marcus T <mtwyford@users.noreply.github.com>
2019-09-18 21:17:14 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*Test file for building and running your program - the main is to support
+*   development in Repl.it
+*
+*/
+
+public class Main {
+
+public static void main ( String[] args) {
+
+
+}  //close main method
+
+} . //close the Main class
+
