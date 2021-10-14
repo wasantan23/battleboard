1a482717939d727b7ee1091a93d43c28a6db2a72
suhanig <60145970+suhanig@users.noreply.github.com>
2021-10-14 09:32:50 -0400

    Oct. 14: Suhani- made initial intro to player. set board dimensions. made comments.

---
+++ UpperBattleship.java
@@ -0,0 +1,20 @@
+public class UpperBattleship { //upper board of battleship
+	public static void main (String[] args) {
+	int[][] board = new int [10][10]; //dimensions of board... 10 x 10
+	  System.out.println (" \nThis is your upper board.");
+		System.out.println ("This will be monitoring the guesses you make on your opponents board. ");
+		System.out.println ("It will mark your hits and misses and notify you when you sink their ship. ");
+		System.out.println ("They have 3 ships of different lengths. These lengths are 2, 3, and 4 units. \n"); //introducing componenets of upper board to player.
+
+
+
+
+
+
+
+
+
+
+
+} //closes static void main 
+} //closes class batttleship
