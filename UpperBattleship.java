146c9fd356921c8b39822ee949765659479401f9
suhanig <60145970+suhanig@users.noreply.github.com>
2021-10-17 14:52:20 -0400

    Added more printed lines. Preparing to add the printed board.

--- UpperBattleship.java
+++ UpperBattleship.java
@@ -1,15 +1,11 @@
 public class UpperBattleship { //upper board of battleship
 	public static void main (String[] args) {
 	int[][] board = new int [10][10]; //dimensions of board... 10 x 10
-	  System.out.println (" \nThis is your upper board.");
+	  System.out.println (" \nThis is your upper board."); 
 		System.out.println ("This will be monitoring the guesses you make on your opponents board. ");
 		System.out.println ("It will mark your hits and misses and notify you when you sink their ship. ");
-		System.out.println ("They have 3 ships of different lengths. These lengths are 2, 3, and 4 units. \n"); //introducing componenets of upper board to player.
-
-
-
-
-
+		System.out.println ("They have 3 ships of different lengths. These lengths are 2, 3, and 4 units. "); //introducing componenets of upper board to player.
+		System.out.println ("Printed below is the board. You will use this to express your guesses. \n");
 
 
 
