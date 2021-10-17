3b47b80e82495d78699c2b6f3cab0d48e372a758
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2021-10-17 15:50:04 -0400

    Oct 17 2021- Asked the user to choose start and position for the different ships

--- Battleship.java
+++ Battleship.java
@@ -1,10 +1,13 @@
- //Tanisha Wasan
+//Tanisha Wasan
+import java.util.Scanner;
 
 public class Battleship {
 
-public static void main (String [] args) {
+	public static void main (String [] args) {
 	
-int [] [] board = new int [10] [10]; // makes an array - 10 rows, 10 colums 
+	int [] [] board = new int [10] [10]; // makes an array - 10 rows, 10 colums 
+
+	Scanner shipPosition = new Scanner(System.in); 
 
 System.out.println ("Hello user! You are going to be playing the game battleship. In this game, you will be playing against the computer.");
 System.out.println("This is a war themed game, where the computer will have to try and guess the location of your ship, and attempt to sink it."); 
@@ -16,13 +19,27 @@ System.out.println("Players will take turns firing shots to atttempt to hit your
 System.out.println("This is the display of the board");
 System.out.println( " [1a,2a,3a,4a,5a,6a,7a,8a,9a,10a] \n [1b,2b,3b,4b,5b,6b,7b,8b,9b,10b] \n [1c,2c,3c,4c,5c,6c,7c,8c,9c,10c] \n [1d,2d,3d,4d,5d,6d,7d,8d,9d,10d] \n [1e,2e,3e,4e,5e,6e,7e,8e,9e,10e] \n [1f,2f,3f,4f,5f,6f,7f,8f,9f,10f] \n [1g,2g,3g,4g,5g,6g,7g,8g,9g,10g] \n [1h,2h,3h,4h,5h,6h,7h,8h,9h,10h] \n [1i,2i,3i,4i,5i,6i,7i,8i,9i,10i] \n [1j,2j,3j,4j,5j,6j,7j,8j,9j,10j] \n");	
 	
-	
-	
-}
+	System.out.println("For the two unit ship, what do you choose as your start position? (Ex. 1a) "); 
+		String shipTwoStart = shipPosition.nextLine(); 
+	System.out.println("For the two unit ship, what do you choose as your end position?");
+		String shipTwoEnd = shipPosition.nextLine(); 
+	System.out.println("For the three unit ship, what do you choose as your start position? (Ex. 1a)"); 
+		String shipThreeStart = shipPosition.nextLine(); 
+	System.out.println("For the three unit ship, what do you choose as your end position?");
+		String shipThreeEnd = shipPosition.nextLine(); 
+	System.out.println ("For the four unit ship, what do you choose as your start position? (Ex. 1a) "); 
+		String shipFourStart = shipPosition.nextLine(); 
+	System.out.println ("For the four unit ship, what do you choose as your end position?");
+		String shipFourEnd = shipPosition.nextLine(); 
 
 }
 	
+}
 	
+
+
+
+
 	
 
 
